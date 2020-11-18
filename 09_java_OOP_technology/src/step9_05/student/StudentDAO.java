0d7d307d6c885cfc3d6f171bffb1e664a895068f
User <User@201-11>
2020-11-18 17:24:45 +0900

    2020-11-18 수 웹자바 실습 미완성

---
+++ 09_java_OOP_technology/src/step9_05/student/StudentDAO.java
@@ -0,0 +1,18 @@
+package step9_05.student;
+
+import java.util.Map;
+
+public class StudentDAO {
+	
+	public void insert(StudentVO st) {
+		StudentRepository.getStDB().put(st.getId(), st);
+	}
+	
+	public StudentVO select(String id) {
+		return StudentRepository.getStDB().get(id);
+	}
+	
+	public Map<String , StudentVO> getStudentDB(){
+		return StudentRepository.getStDB();
+	}
+}
